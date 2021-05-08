f150c3fbc1efe32d296b8e0afe312e479172a122
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-05-08 18:38:52 +0000

    revert

---
+++ frontend/client/src/stories/NavBar.stories.js
@@ -0,0 +1,13 @@
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+// import "index.scss";
+// import NavBar from "../../components/NavBar";
+import NavBarItem from "../components/NavBar/NavBarItem";
+
+export const NavBarComponent = () => <NavBarItem />
+
+// export default {
+//   title: 'Components/NavBarItem',
+//   component: NavBarItem
+// }
---
+++ frontend/client/src/stories/Products.stories.js
@@ -0,0 +1,10 @@
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+// import "index.scss";
+
+import ProductList from "../components/Products/ProductList";
+import ProductItem from "../components/Products/ProductItem";
+
+export const ProductListComponent = () => <ProductList />
+export const ProductItemComponent = () => <ProductItem />
