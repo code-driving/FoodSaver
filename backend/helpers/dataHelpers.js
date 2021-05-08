e50319ee9a09f1b03faaa8aff7d928c51b6e73f6
Gabriel Chen <gabriel.jchen@gmail.com>
2021-05-08 15:08:30 -0400

    commit before branch swithc

--- backend/helpers/dataHelpers.js
+++ backend/helpers/dataHelpers.js
@@ -1,32 +1,30 @@
-const { getMaxListeners } = require("node:process");
+// const { getMaxListeners } = require("node:process");
 
 const getProductsByUsers = (usersProducts) => {
   const productsByUsers = {};
 
   for (let product of usersProducts) {
-      if (!productsByUsers[product.user_id]) {
-          productsByUsers[product.user_id] = {
-              userId: product.user_id,
-              name: product.name,
-              email: product.email,
-              products: [],
-          };
-      }
+    if (!productsByUsers[product.user_id]) {
+      productsByUsers[product.user_id] = {
+        userId: product.user_id,
+        name: product.name,
+        email: product.email,
+        products: [],
+      };
+    }
 
-      productsByUsers[product.user_id].products.push({
-          name: product.name,
-          expiration_date : product.expiration_date
-      });
-
+    productsByUsers[product.user_id].products.push({
+      name: product.name,
+      expiration_date: product.expiration_date,
+    });
   }
 
   return Object.values(productsByUsers);
 };
 
 module.exports = {
-  getProductsByUsers
+  getProductsByUsers,
 };
-
 
 // 1 : {
 //   userid:1,
