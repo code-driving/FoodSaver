d715a369f77d5b8602afbbd3a82a9c2a20087ab0
Tetyana Titovets <tittan7@gmail.com>
2021-05-07 20:29:10 -0400

    Create basic routing in React

--- frontend/client/src/components/Application.js
+++ frontend/client/src/components/Application.js
@@ -8,8 +8,45 @@ import {
 
 export default function Application(props) {
   return (
-    <div>
-      <h1>Hello from main page</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Products</Link>
+            </li>
+            <li>
+              <Link to="/recipes">Recipes</Link>
+            </li>
+            <li>
+              <Link to="/quantities">Summary</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Products />
+          </Route>
+          <Route path="/recipes">
+            <Recipes />
+          </Route>
+          <Route path="/quantities">
+            <Quantities />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Products() {
+  return <h2>Home page with all products</h2>;
+}
+
+function Recipes() {
+  return <h2>Recipes</h2>;
+}
+
+function Quantities() {
+  return <h2>Quantities</h2>;
 }
