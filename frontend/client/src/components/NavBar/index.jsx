d94660083685231bc6f95be41deb18fc1f0b41f0
Tetyana Titovets <tittan7@gmail.com>
2021-05-10 02:39:00 -0400

    Continue work on Login and Navbar

--- frontend/client/src/components/NavBar/index.jsx
+++ frontend/client/src/components/NavBar/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-
 export default function NavBar() {
   return (
       <nav>
         <ul>
-          <li><Link to="/">Products</Link></li>
-          <li><Link to="/recipes">Recipes</Link></li>
-          <li><Link to="/summaries">Summary</Link></li>
-          <li><Link to='/login'>Login</Link></li>
+          <li><Link to="/">products</Link></li>
+          <li><Link to="/recipes">recipes</Link></li>
+          <li><Link to="/summaries">summary</Link></li>
+          <li><Link to='/login'>login</Link></li>
         </ul>
       </nav>
   )
