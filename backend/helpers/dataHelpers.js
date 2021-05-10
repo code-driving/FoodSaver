4acb8cff2a9cb1b648ef56d8b5890cf35cc0fb82
Tetyana Titovets <tittan7@gmail.com>
2021-05-10 13:51:05 -0400

    resolve conflicts, accepted changes

--- backend/helpers/dataHelpers.js
+++ backend/helpers/dataHelpers.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-=======
-// const { getMaxListeners } = require("node:process");
-
->>>>>>> 6c0341075e61592c2e7f4dda8902cd17379a5b30
 const getProductsByUsers = (usersProducts) => {
+  console.log("usersProducts===> ", usersProducts)
   const productsByUsers = {};
   for (let product of usersProducts) {
-<<<<<<< HEAD
       if (!productsByUsers[product.user_id]) {
           productsByUsers[product.user_id] = {
               user_Id: product.user_id,
@@ -29,31 +24,15 @@ const getProductsByUsers = (usersProducts) => {
       });
 
 
-=======
-    if (!productsByUsers[product.user_id]) {
-      productsByUsers[product.user_id] = {
-        userId: product.user_id,
-        name: product.name,
-        email: product.email,
-        products: [],
-      };
-    }
-
-    productsByUsers[product.user_id].products.push({
-      name: product.name,
-      expiration_date: product.expiration_date,
-    });
->>>>>>> 6c0341075e61592c2e7f4dda8902cd17379a5b30
   }
 
   return Object.values(productsByUsers);
 };
 
-<<<<<<< HEAD
 const AppendRecipes = (savedRecipe,formattedProducts) => {
   
   for (let productsobject of formattedProducts) {
-     for (let recipe of savedRecipe) {
+      for (let recipe of savedRecipe) {
        if (productsobject.user_Id === recipe.user_id)
         productsobject['recipes'].push({
         recipie_name: recipe.recipie_name,
@@ -65,10 +44,10 @@ const AppendRecipes = (savedRecipe,formattedProducts) => {
 };
 
 const AppendSummary = (Summaries,combinedData) => {
- 
+
   for (let productsobject of combinedData) {
-     for (let summary of Summaries) {
-       if (productsobject.user_Id === summary.user_id) {
+      for (let summary of Summaries) {
+        if (productsobject.user_Id === summary.user_id) {
           productsobject['summary'].push({
           id: summary.id,
           name: summary.name,
@@ -94,22 +73,3 @@ module.exports = {
   AppendSummary
 };
 
-=======
-module.exports = {
-  getProductsByUsers,
-};
-
-// 1 : {
-//   userid:1,
-//   name:getMaxListeners
-//   email:asd@getMaxListeners,
-//   products: [
-//     potatos : {
-//       quantity:2,
-//     },
-//     tomatos,
-//     cheese
-//   ],
-//   recipes:{}
-// }
->>>>>>> 6c0341075e61592c2e7f4dda8902cd17379a5b30
