bcd83735f70b797dcaa7fd42b50c4d45ceae9cdb
abdul5890575 <abdulrehman.shoukat@gmail.com>
2021-05-10 22:34:09 -0400

    spoon api call

---
+++ frontend/client/src/helpers/ingredientsToString.js
@@ -0,0 +1,15 @@
+export function ingredientsToString (ingredientsArray) {
+let ingredientString = ''
+count = 0
+
+for (item of ingredientsArray) {
+   count++
+   if (count ===  ingredientsArray.length){
+     ingredientString += item
+   } else {
+      ingredientString += item +',+'
+   }
+
+}
+  return ingredientString 
+}
