54db6cc0bcb918ab1dcebc8722e1ca8771804ac3
abdul5890575 <abdulrehman.shoukat@gmail.com>
2021-05-12 14:40:39 -0400

    api second added

--- frontend/client/src/components/Recipes/Recipedetail.jsx
+++ frontend/client/src/components/Recipes/Recipedetail.jsx
@@ -6,29 +6,30 @@ import useRecipeDetails from '../../hooks/useRecipeDetails'
 //we will need onSubmit as a prop to handle recipe submit event. This could be a button in RecipeList
 
 export default function RecipeDetail(props) {
-  const [ details, setDetails ] = useState()
+
   let recipie_id = props.match.params.id
   const recipeDetails  = useRecipeDetails(recipie_id)
  
   let steps = []
+  steps = recipeDetails.steps[0]['steps']
+  let img =recipeDetails.info['image']
   
-  if(recipeDetails.length > 0 ) {
-    steps = recipeDetails[0]['steps']
-  }
+  console.log('aaaaaaaa',recipeDetails)
+  // if(recipeDetails.length > 0 ) {
+  //   console.log('bbbbbbbb',recipeDetails)
+  //   // steps = recipeDetails[0]['steps']
+  // }
 
   const EachStep= steps.map((step, index) => {return <li key={index}>{step.step}</li>})
   
   return (
     <section>
      Details
- 
+     <img src={img} alt={"food image"}></img>
     <ul>
       <li>test</li>
       {EachStep}
     </ul> 
- 
-      {/* <h1>{recipeDetails && recipeDetails[0]['steps'][0]['step']}</h1> */}
-  
-    </section>
+   </section>
   )
 }
--- frontend/client/src/hooks/useRecipeDetails.jsx
+++ frontend/client/src/hooks/useRecipeDetails.jsx
@@ -2,18 +2,22 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useRecipesDetailsApi(recipe_id) {
-  const [details, setDetails] = useState([]);
+  const [details, setDetails] = useState({ steps : [], info:[]});
   
  
   useEffect(() => {
     if (recipe_id) {
       const APIKEY = process.env.API_KEY;
       const url = `https://api.spoonacular.com/recipes/${recipe_id}/analyzedInstructions?apiKey=f8973fa0549347b38d9ffd74077d423f&boolean=false`
-      axios
-        .get(url)
-        .then(res =>  setDetails(res.data))
-      
-    }
+      const url2 = `https://api.spoonacular.com/recipes/${recipe_id}/information?apiKey=f8973fa0549347b38d9ffd74077d423f&boolean=false`
+      Promise.all([
+      axios.get(url),
+      axios.get(url2)
+      ]).then((res) => {
+        console.log(res)
+        setDetails(prev => ({...prev, steps:res[0].data , info:res[1].data}));
+      })
+    }     
   }, [recipe_id])
 
   return  details 
@@ -22,3 +26,15 @@ export default function useRecipesDetailsApi(recipe_id) {
 
 //<input type="button" disabled={sendRequest} onClick={() => setDetails(true)}
 
+// Promise.all([
+//   axios.get(`/api/users`),
+//   axios.get(`/api/products/${localId}`),
+//   axios.get(`/api/recipes`),
+//   axios.get(`/api/summary`),
+// ]).then((all) => {
+//   setState(prev => ({...prev, Day:"Monday", days: all[0].data, appointments: all[1].data ,interviewers : all[2].data }));
+//   })
+// }, [])
+
+
+// https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
