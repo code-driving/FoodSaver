1fce16c572f048b43988ce958b4eb47935a8f727
Tetyana Titovets <tittan7@gmail.com>
2021-05-14 10:56:13 -0400

    delete feauture

--- frontend/client/src/components/Favourites/FavouriteItem.jsx
+++ frontend/client/src/components/Favourites/FavouriteItem.jsx
@@ -1,13 +1,10 @@
-
 import { useState, Fragment, useEffect } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
-// import { useParams } from 'react-router-dom';
+
 
 export default function FavouriteItem(props) {
 const { recipes, deleteRecipe } = props
-// const { id } = useParams();
-// console.log("id from FavouriteItem", id);
 
   return (
     <>
--- frontend/client/src/components/Favourites/FavouriteList.jsx
+++ frontend/client/src/components/Favourites/FavouriteList.jsx
@@ -4,8 +4,8 @@ import FavouriteItem from './FavouriteItem'
 export default function FavouriteList(props) {
   const { recipes, deleteRecipe } = props;
   
-  const favouriteRecipes = Array.isArray(recipes) && recipes.map((favourite) => <FavouriteItem key={favourite.title} {...favourite} deleteRecipe={deleteRecipe}/>)
-  
+  const favouriteRecipes = Array.isArray(recipes) && recipes.map((favourite, index) => <FavouriteItem key={index} {...favourite} deleteRecipe={deleteRecipe}/>)
+
   return (
     <div>
       {favouriteRecipes}
