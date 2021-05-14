21cb2b93701c7d04a5e714fd542c09acee96d9ae
abdul5890575 <abdulrehman.shoukat@gmail.com>
2021-05-13 20:30:32 -0400

    backup

--- frontend/client/src/components/Recipes/Recipedetail.jsx
+++ frontend/client/src/components/Recipes/Recipedetail.jsx
@@ -17,8 +17,8 @@ export default function RecipeDetail(props) {
   useEffect(() => {
     if (recipe_id) {
       const APIKEY = process.env.API_KEY;
-      const url = `https://api.spoonacular.com/recipes/${recipe_id}/analyzedInstructions?apiKey=f8973fa0549347b38d9ffd74077d423f&boolean=false`
-      const url2 = `https://api.spoonacular.com/recipes/${recipe_id}/information?apiKey=f8973fa0549347b38d9ffd74077d423f&boolean=false`
+      const url = `https://api.spoonacular.com/recipes/${recipe_id}/analyzedInstructions?apiKey=fe944c0a7ca548aa96c6ac698fdbdf91&boolean=false`
+      const url2 = `https://api.spoonacular.com/recipes/${recipe_id}/information?apiKey=fe944c0a7ca548aa96c6ac698fdbdf91&boolean=false`
       Promise.all([
       axios.get(url),
       axios.get(url2)
