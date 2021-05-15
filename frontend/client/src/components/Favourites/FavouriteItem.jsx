020f718a512d7c8ca6a60b545a37b7ccf2f58f51
Gabriel Chen <gabriel.jchen@gmail.com>
2021-05-14 21:45:26 -0400

    move img into link

--- frontend/client/src/components/Favourites/FavouriteItem.jsx
+++ frontend/client/src/components/Favourites/FavouriteItem.jsx
@@ -1,17 +1,18 @@
 import { useState, Fragment, useEffect } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export default function FavouriteItem(props) {
-const { recipes, deleteRecipe } = props
+  const { recipes, deleteRecipe } = props;
 
   return (
     <>
       <h1>Recipe Details</h1>
-      <Link to={`/recipes/${props.recipe_id}`}><ul>{props.recipie_name}</ul></Link>
-      <img src={props.imagesrc} alt={"food image"} class="recipe-image"></img>
+      <Link to={`/recipes/${props.recipe_id}`}>
+        <ul>{props.recipie_name}</ul>
+        <img src={props.imagesrc} alt={"food image"} class="recipe-image"></img>
+      </Link>
       <button onClick={() => deleteRecipe(props.recipe_id)}>del</button>
     </>
-  )
+  );
 }
