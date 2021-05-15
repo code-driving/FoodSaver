b0aad3ed48fc6069ed14ac824e74f89e8e3e3807
Gabriel Chen <gabriel.jchen@gmail.com>
2021-05-14 20:58:39 -0400

    add disabled attribute to recipes form

--- frontend/client/src/components/Recipes/Form.jsx
+++ frontend/client/src/components/Recipes/Form.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 import "./Recipes.scss";
 
-
 export default function Form(props) {
-  const [formData,setFormData] = useState({ingredients:''})
-  
+  const [formData, setFormData] = useState({ ingredients: "" });
+
   const handleSubmit = (event) => {
-    event.preventDefault()
-    props.onSubmit(formData)
-  }
+    event.preventDefault();
+    props.onSubmit(formData);
+  };
 
   const handleChange = (event) => {
-    const { name, value } = event.target
-    setFormData({ ...formData, [name]: value })
-    
-  }
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   return (
     <form className="Form" onSubmit={handleSubmit}>
       <h1>Enter ingredients to search</h1>
       <input
-          type="text"
-          name='ingredients'
-          value={formData.ingredients}
-          onChange={handleChange}
-          placeholder='apples,oranges'
-        ></input>
-        <button onClick={handleSubmit}>Go!</button>
+        type="text"
+        name="ingredients"
+        value={formData.ingredients}
+        onChange={handleChange}
+        placeholder="apples,oranges"
+      ></input>
+      <button disabled={!formData.ingredients} onClick={handleSubmit}>
+        Go!
+      </button>
     </form>
-  )
-
+  );
 }
