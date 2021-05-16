d935873f529d0cf8508b6d87b93e86b42d7760db
Tetyana Titovets <tittan7@gmail.com>
2021-05-15 22:50:42 -0400

    add arrow to the recipe detail

--- frontend/client/src/components/Recipes/Recipedetail.jsx
+++ frontend/client/src/components/Recipes/Recipedetail.jsx
@@ -5,6 +5,7 @@ import "./details.scss";
 import useApplicationData from '../../hooks/useApplicationData'
 import { useParams, Redirect } from 'react-router-dom';
 import "./Recipes.scss"
+import ScrollToTop from "../ScrollToTop/index"
 
 export default function RecipeDetail(props) {
   const [redirect, setRedirect] = useState(false);
@@ -96,6 +97,7 @@ export default function RecipeDetail(props) {
       <h2 style={{margin: '1rem 0 1rem 0'}}>Instructions</h2>
       <ul className="recipe_detail">{EachStep}</ul>
       <Link to={`/favourites`}><button className="button" style={{marginTop: '1.3rem'}} onClick={() => setRecipe(value)}>save</button></Link>
+      <ScrollToTop />
     </section>
   );
 }
